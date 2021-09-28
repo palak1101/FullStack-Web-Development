51431a09d30eec1ee25085860941ade866244518
Palak Tiwari <palaktiwari156@gmail.com>
2021-09-28 18:57:07 +0530

    primitive datatypes

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log("Hello World!")
+// console.warn("Warning...")
+// console.error("Error!!!")
+
+//3 Datatypes (Primitive)
+//Strings
+console.log(typeof "starlight")
+console.log(typeof 'd')
+
+
+//Numbers- only numerical datatype
+console.log(typeof 11)
+console.log(typeof 0.22)
+console.log(typeof 4562212358968482)
+console.log(typeof -9)
+
+
+//Boolean
+console.log(true)
+console.log(typeof false)
