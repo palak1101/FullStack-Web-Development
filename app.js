3ebb97599e40e42f1738a2cfb47eb2e571dac4b4
Palak <palaktiwari156@gmail.com>
2021-09-30 19:15:11 +0530

    functions

--- app.js
+++ app.js
@@ -1,25 +1,78 @@
-// document. -object
+//1.
+function greet(Name, age){
+    console.log(Name, age)
+    console.log('Hello')
+}
 
-// var heading = document.getElementById('heading')
-// console.log(heading)     you're tring to access element that hasn't been declared before script tag, hence it couldn't recognize it.
+greet('Palak', 20)
 
-// var heading = document.getElementById('heading')
-// console.log(heading)
-// heading.innerHTML = "Hi from JS"
-// console.log(typeof heading) 
+//2.
+// function greet(Name, age){
+//     console.log(age)
+//     console.log('Hello')
+// }
 
-var heading = document.getElementById('heading')
-var para = document.getElementsByClassName('para')
-var tags = document.getElementsByTagName('a')
-var query = document.querySelector('#heading')
+// greet('Palak') -value not assigned while calling hence undefined
 
-console.log(heading)
-console.log(para)
-console.log(para.length)
-console.log(para[0])
-console.log(para[1])
-console.log(tags)
-console.log(query)
+//3. Default value
+function greet(Name, age = 19){
+    console.log(Name, age)
+    console.log('Hello')
+}
 
-//para.push('star')
+greet('Palak')   
+
+//4.
+// function sum(a,b){
+//     console.log(a+b)  -output = NaN (not a number)
+// }
+
+// sum(3)
+
+//5.
+function add(a = 0, b = 0){
+    var sum = a + b
+    return sum
+}
+
+var result = add(10, 20)
+console.log(result)
+
+//console.log(add(10, 20))
+
+//6.
+// function add(a = 0, b = 0){
+//     var sum = a + b
+//     return sum
+// }
+// console.log(sum)  -reference error since it(sum) is out of scope not global its local to function.
+
+// var result = add(10, 20)
+// console.log(result)
+
+//Function Expression
+
+function add(a = 0, b = 0){
+    var sum = a + b
+    return sum
+}
+
+var add = function (a = 0, b = 0){
+    var sum = a + b
+    return sum
+}
+
+
+console.log(add(10,20))
+
+//Arrow Function
+
+var add = (a = 0, b = 0) => {     //-It saves time , no need to write function keyword everytime.
+    var sum = a + b
+    return sum
+}
+
+
+console.log(add(10,20))
+
 
