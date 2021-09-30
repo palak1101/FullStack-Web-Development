92240fc1504a3a1a9ebaa0c53663364e3dee3e45
Palak <palaktiwari156@gmail.com>
2021-09-30 19:48:09 +0530

    callback function

--- app.js
+++ app.js
@@ -1,78 +1,38 @@
+//Shorthands of arrow function-
+
 //1.
-function greet(Name, age){
-    console.log(Name, age)
-    console.log('Hello')
-}
-
-greet('Palak', 20)
-
-//2.
-// function greet(Name, age){
-//     console.log(age)
-//     console.log('Hello')
+//  var add = (a = 0, b = 0) => {
+//     return a+b
 // }
 
-// greet('Palak') -value not assigned while calling hence undefined
+// 2. 
+// var add = (a = 0, b = 0) => a + b
 
-//3. Default value
-function greet(Name, age = 19){
-    console.log(Name, age)
-    console.log('Hello')
-}
+// console.log(add(10,20))
 
-greet('Palak')   
 
-//4.
-// function sum(a,b){
-//     console.log(a+b)  -output = NaN (not a number)
+//3.
+// const greet = name => 'Hi!' + name
+
+// console.log(greet('Palak'))
+
+
+
+
+//CallBack Functions
+var arr = ['My', 'Name', 'is', 'StarLight']
+
+//To iterate - for loop
+// for (var i = 0; i < arr.length; i++){
+//     console.log(arr[i])
 // }
 
-// sum(3)
-
-//5.
-function add(a = 0, b = 0){
-    var sum = a + b
-    return sum
-}
-
-var result = add(10, 20)
-console.log(result)
-
-//console.log(add(10, 20))
-
-//6.
-// function add(a = 0, b = 0){
-//     var sum = a + b
-//     return sum
+// var callbackFunction = (element, index) => {
+//     console.log(element, index)
 // }
-// console.log(sum)  -reference error since it(sum) is out of scope not global its local to function.
-
-// var result = add(10, 20)
-// console.log(result)
-
-//Function Expression
-
-function add(a = 0, b = 0){
-    var sum = a + b
-    return sum
-}
-
-var add = function (a = 0, b = 0){
-    var sum = a + b
-    return sum
-}
+//arr.forEach(callbackFunction)
 
 
-console.log(add(10,20))
-
-//Arrow Function
-
-var add = (a = 0, b = 0) => {     //-It saves time , no need to write function keyword everytime.
-    var sum = a + b
-    return sum
-}
-
-
-console.log(add(10,20))
-
-
+arr.forEach((element, index) => {  -shorthand of above two steps
+    console.log(element, index)
+})
