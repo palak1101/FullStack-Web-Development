e8fa022b44d4e2468381d9e70bb6f9ec6730a9c2
Palak <palaktiwari156@gmail.com>
2021-10-06 15:18:05 +0530

    append list - JS

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var body = document.querySelector('body')
+//How to add textNode to list dynamicaaly when array is created manually and automatically
 
-var heading = document.getElementById('heading')
-heading.innerHTML = "I was changed by JS"
-console.log(heading)
-
-
-heading.style = "color:red; font-size:3rem"
+var list = document.querySelector('ul')
+//console.log(list)
 
 
-heading.classList.add("four")
-heading.classList.remove('one')
-console.log(heading.classList)
+var arr = ["Go to park", "cook oatmeal", "Eat"]
 
+arr.push("Complete the assignment")
+arr.push("Sleep")
 
-body.classList.add('dark')
-
+for(var i=0; i<arr.length; i++){
+    //console.log(arr[i])
+    var element = document.createElement('li')
+    var textNode = document.createTextNode(arr[i])
+    element.appendChild(textNode)
+    list.appendChild(element)
+}
