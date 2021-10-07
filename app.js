ec8d4cf32f3295dc887ad9ffc7e89c45de1f8a43
Palak <palaktiwari156@gmail.com>
2021-10-07 21:00:15 +0530

    dark mode

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
+var body = document.querySelector('body')
+var heading = document.querySelector('#heading')
 var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
 
-//check - console.log(button, input, list)
-// var callbackfunction = () => {
-//     console.log("clicked")
-// }
+console.log(heading, button)
 
-// button.addEventListener('click', () => {
-//     console.log("clicked")
-// })
-
-// button.addEventListener('click', (event) => {
-//     console.log(event)
-//     console.log(event.target)
-//     console.log(input.value)
-// })
-
-
-var callbackfunction = (event) => {
-    const inputValue = input.value
-    const element = document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
+var callbackfunction = () => {
+    body.classList.toggle('dark')
 }
 
 button.addEventListener('click', callbackfunction)
-
