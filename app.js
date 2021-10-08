2f0b3e1e4c16cfac6a5ab8819e6da7ddde18892e
Palak <palaktiwari156@gmail.com>
2021-10-08 18:52:54 +0530

    objects

--- app.js
+++ app.js
@@ -1,81 +1,69 @@
-// let and var both are variables, gets reassigned.
-let a = 5
-var b = 89
+// This : points to window object => parent object right now it is window that we're seeing (chrome's window.
+console.log(this.alert("Hi!"))
 
-console.log(a, b)
+//Create an object and add properties and methods to object.
+let manufacturer = "Ferrari"
 
-a = 12
-b=23
-console.log(a, b)
-
-
-// Difference between let and var?
-//let = local scope & var = global scope
-if(true){
-    var Name = "Star"
-    console.log(Name)
-}
-
-console.log(Name)
-
-
-
-if(true){
-    let Name = "Star"
-    console.log(Name)
-}
-
-console.log(Name)
-
-
-// example-2
-for(let i=0; i<10; i++){
-    console.log("loop ran")
-}
-
-console.log(i)
-
-
-// example-3 (Scopes can be nested)
-if(true){
-    var Name = "Light"
-    if(true){
-        console.log(Name)
+let car = {
+    name: "C-Class",
+    manufacturer: "Mercedes",
+    print: function () {
+        console.log(this)
+        let manufacturer = "BMW"
+        console.log(manufacturer)
     }
 }
 
-// -> You can access parent scope within child but not vice-versa.
-if(true){
-    console.log(Name)
-    if(true){
-        var Name = "Light"
+car.print()
+
+console.log(car)
+console.log(typeof car)
+
+console.log(car.name)
+console.log(car.manufacturer)
+console.log(car)
+
+
+//Q) How can methods access object properties then?
+let car = {
+    name: "C-Class",
+    manufacturer: "Mercedes",
+    print: function () {
+        console.log(this)
+        console.log(car.manufacturer, car.name)
+        console.log(this.name + " was manufactured by " + this.manufacturer)
+        console.log(`${this.name}  was created by ${this.manufacturer}`)
+        
     }
 }
 
-
-// example- 4
-// In case of function it is different - there is only local scope whether you use - var, let or const.
-
-const print = () => {
-    var Name = "Ferrari"
-    return Name
-}
-
-print()
-console.log(Name)
+car.print()
 
 
-//-> Nested scope in function works.
-const print = () =>{
-    var Name = "Moon"
-    if (true){
-        console.log(Name)
+// 'this' keyword doesn't work with object's arrow function-
+let car = {
+    name: "C-Class",
+    manufacturer: "Mercedes",
+    print: () => {
+        console.log(this)
+        //console.log(car.manufacturer, car.name)
+        //console.log(this.name + " was manufactured by " + this.manufacturer)
+        console.log(car.name + " was created by " + car.manufacturer)
+        
     }
-
-    return Name
 }
 
-print()
+car.print()
+
+
+// Object Methods-
+//Q) What is 'Object' object here? - a global object and created object are subset of this Object.
+console.log(car)
+
+
+console.log(Object.keys(car))
+console.log(Object.values(car))
+console.log(Object.hasOwnProperty('name'))
 
 
 
