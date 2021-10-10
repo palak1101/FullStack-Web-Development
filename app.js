5ecc6405d6abc1d46b810a740cb7af0285e1d0a9
Palak <palaktiwari156@gmail.com>
2021-10-11 01:21:52 +0530

    recipe app - core

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const button = document.querySelector('button')
 const input = document.querySelector('input')
+const parent = document.querySelector('div.grid')
 
-// const getData = async () => {
-//     const url = 'https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=Pizza'
 
-//     const response = await fetch(url)
-//     const data = await response.json()
-//     console.log(data)
-// }
-
-// Add eventListener to the button-
-// button.addEventListener('click', () => {
-//     getData()
-// })
-
+// To get any item's receipe-
 
 const getData = async (item) => {
     console.log(item)
@@ -21,7 +11,34 @@ const getData = async (item) => {
     console.log(url)
     const response = await fetch(url)
     const data = await response.json()
-    console.log(data.hits)
+    const recipies = data.hits
+    //console.log(recipies)
+    recipies.forEach((item) => {
+        //console.log(item)
+        //console.log(item.recipe.label)
+        const {recipe} = item
+        //console.log(recipe)
+        const {label, image, source, calories} = recipe
+        //console.log(label, image, source, calories)
+
+        const template = `
+                        <img class="card__image" src="${image}" />
+                        <div class="card__data">
+                            <div class="card__info">
+                                <h2>${label}</h2>
+                                <p>From: ${source}</p>
+                            </div>
+                            <h3 class="Calories:">${calories.toFixed(2)}</h3>
+                            <button class="card__add">+</button>
+                        </div>
+                    `
+        const newCard = document.createElement('article')
+        newCard.setAttribute('class', 'card')
+        newCard.setAttribute('style', 'margin-bottom:60px')
+        newCard.innerHTML = template
+        parent.appendChild(newCard)
+
+    })
 }
 
 
