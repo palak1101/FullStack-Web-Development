6ce5056f4558e25d5c92805d0eda4c3bdaf09830
Palak <palaktiwari156@gmail.com>
2021-10-10 23:46:15 +0530

    get data from api- particular or any on search

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-// Async & Await-
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+
+// const getData = async () => {
+//     const url = 'https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=Pizza'
+
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     console.log(data)
+// }
+
+// Add eventListener to the button-
+// button.addEventListener('click', () => {
+//     getData()
+// })
 
 
-// fetch('https://jsonplaceholder.typicode.com/users')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         data.forEach((user) => {
-//             console.log(user)
-//         })
-//     })
-
-const fetchData = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
+const getData = async (item) => {
+    console.log(item)
+    const url =  `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+    console.log(url)
+    const response = await fetch(url)
     const data = await response.json()
-
-    console.log(data)
+    console.log(data.hits)
 }
 
-fetchData()
 
+// Add eventListener to the button-
+button.addEventListener('click', (e) => {
+    getData(input.value)
+})
 
-// const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
-// const data = await response.json()
-
-// console.log(data)
