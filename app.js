a606d1ed5b6265dd2c26b8fd177efe406a6411f6
Palak <palaktiwari156@gmail.com>
2021-10-10 17:52:03 +0530

    async & await

--- app.js
+++ app.js
@@ -1,47 +1,28 @@
-// To display fetched data-
-
-const table = document.querySelector('table')
-console.log(table)
+// Async & Await-
 
 
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        data.forEach((user) => {
-            table.appendChild(createRow(user))
-        }) 
-        
-    })
-
-
-const createRow = (user) =>{
-    let newRow = document.createElement('tr')
-    let element = `
-    <td>${user.id}</td>
-    <td>${user.name}</td>
-    <td>${user.email}</td>
-    <td>${user.username}</td>`
-    
-    newRow.innerHTML = element
-    return newRow
-}
-
-
-// fetch("https://jsonplaceholder.typicode.com/users")
+// fetch('https://jsonplaceholder.typicode.com/users')
 //     .then(res => res.json())
 //     .then(data => {
 //         console.log(data)
 //         data.forEach((user) => {
-//             let newRow = document.createElement('tr')
-//             let element = `
-//             <td>${user.id}</td>
-//             <td>${user.name}</td>
-//             <td>${user.email}</td>
-//             <td>${user.username}</td>`
-
-//             newRow.innerHTML = element
-//             table.appendChild(newRow)
-//         } ) 
-        
+//             console.log(user)
+//         })
 //     })
+
+const fetchData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    const data = await response.json()
+
+    console.log(data)
+}
+
+fetchData()
+
+
+// const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+// const data = await response.json()
+
+// console.log(data)
