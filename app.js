fcbee4e59ebd042a9c817a43177da94063f32e2f
Palak <palaktiwari156@gmail.com>
2021-10-11 15:49:10 +0530

    basics of node.js

--- app.js
+++ app.js
@@ -1,49 +1,13 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const parent = document.querySelector('div.grid')
+//console.log(module)
+
+//console.log((require('./index.js')))
+
+// 1. Access exports object of a module or whole file from another js file using require built-in method.
+const stuff = require('./index')
+console.log(stuff)
+
+//console.log(something)
 
 
-// To get any item's receipe-
 
-const getData = async (item) => {
-    console.log(item)
-    const url =  `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
-    console.log(url)
-    const response = await fetch(url)
-    const data = await response.json()
-    const recipies = data.hits
-    //console.log(recipies)
-    recipies.forEach((item) => {
-        //console.log(item)
-        //console.log(item.recipe.label)
-        const {recipe} = item
-        //console.log(recipe)
-        const {label, image, source, calories} = recipe
-        //console.log(label, image, source, calories)
-
-        const template = `
-                        <img class="card__image" src="${image}" />
-                        <div class="card__data">
-                            <div class="card__info">
-                                <h2>${label}</h2>
-                                <p>From: ${source}</p>
-                            </div>
-                            <h3 class="Calories:">${calories.toFixed(2)}</h3>
-                            <button class="card__add">+</button>
-                        </div>
-                    `
-        const newCard = document.createElement('article')
-        newCard.setAttribute('class', 'card')
-        newCard.setAttribute('style', 'margin-bottom:60px')
-        newCard.innerHTML = template
-        parent.appendChild(newCard)
-
-    })
-}
-
-
-// Add eventListener to the button-
-button.addEventListener('click', (e) => {
-    getData(input.value)
-})
 
