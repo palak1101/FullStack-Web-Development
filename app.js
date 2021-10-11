f16b0b30b6b69744bfe0dd1562ed1336f0feaa0d
Palak <palaktiwari156@gmail.com>
2021-10-11 18:59:09 +0530

    module - path methods

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const div = document.querySelector('div')
+// I. Module - path
 
+const path = require('path')
+//console.log(path)
 
-const arr = [1, 2, 3, 4, 5]
-const val = JSON.stringify(arr)  //converts array/anything to string
-localStorage.setItem('val', JSON.stringify(arr))
-console.log(val)
-console.log(arr)
+//1.
+console.log(path.resolve('./utils.js'))   //converts relative to absolute path from root of utils.js file
+console.log(path.relative('/', "./utils.js"))  // absolute path using relative
 
-console.log(localStorage.getItem('val'))
-console.log(JSON.parse(localStorage.getItem('val')))
+console.log(path.relative('./GIT CONTRIBUTE', "./GIT CONTRIBUTE/FullStack-Track/utils.js"))  //converts absolute path to relative
+
+//2.
+console.log(path.extname('./app.js'))  //shows extension name
+
+//3.
+console.log(path.dirname('./FullStack-Track/utils.js'))  // will give ypu parent directory
+
+//4.
+console.log(__dirname) //gives current directory in which your file you're using right now.
+console.log(path.join(__dirname, 'app.js'))  //joins file with current directory.
