ac2828fee7d0641561a8c5f81d400f15ad06d35c
Palak <palaktiwari156@gmail.com>
2021-10-13 15:30:26 +0530

    created http server & sent response over it to client side

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
-//working of nodemon
-// console.log("message")
-// console.log("After nodemom")
-// console.log("Hi!")
-
-//Creating a HTTP Server-
 const http = require('http')
 //console.log(http)
 
-const fs = require('fs')
-const path = require('path')
+
+// create a http server and send response over it-
 
 const server = http.createServer((request, response) => {
-    // console.log(request.url)
-    // console.log(request.headers)
     //console.log(request.method)
-    const markup = fs.readFileSync(path.resolve('./index.html'))
-    response.write(markup)
-    //const path = request.url
-    //response.write(`You made a request to ${path}`)
-    //response.write('hi!')
-    //response.write(`<h2>I'm a heading</h2>`)
+    const path = request.url
+    response.write(`You made a request to ${path}`)
     response.end()
 })
+
+//console.log(server)
 
 server.listen(3000, () => {
     console.log(`Server listening at PORT: ${3000}`)
 })
 
-//console.log(server)
+
