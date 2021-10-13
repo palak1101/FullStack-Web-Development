49f0f9a678a19645345b5b114efa3a353446a750
Palak <palaktiwari156@gmail.com>
2021-10-13 19:44:49 +0530

    validating request with express

--- server.js
+++ server.js
@@ -2,23 +2,45 @@
 // creating server using express-
 
 const express = require('express')
+console.log(express)
+const PORT = 3000
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log(req.url)
-    res.send("This is a response")
+// app.get('/', (req, res) => {
+//     res.send('Server up and running')
+// })
+
+
+//1. If a request is legit that means get request which we have allowed - that means from thunderclient but not chrome.
+// app.get('/', (req, res) => {
+//     console.log(req)
+//     if(req.headers['user-agent'] ==="Thunder Client (https://www.thunderclient.io)") res.send('GET')
+//     else res.send("BLOCKED")
+// })
+
+
+
+//2. checks if request is from thunder client if yes moves it ahead to callback otherwise show blocked.
+const verify = (req, res, next) => {
+    console.log(req)
+    if(req.headers['user-agent'] ==="Thunder Client (https://www.thunderclient.io)") next()
+    else res.send("BLOCKED")
+}
+
+app.get('/', verify, (req, res) => {
+    res.send("VERIFIED")
 })
 
-app.get('/login', (req, res) => {
-    console.log(req.url)
-    res.send("This is a Login Page")
-})
 
-app.get('/signup', (req, res) => {
-    console.log(req.url)
-    res.send("This is a Signup Page")
-})
 
-app.listen(3000, () => {
-    console.log("server listening at PORT 3000")
+// app.post('/', (req, res) => {
+//     res.send("Login form")
+// })
+
+// app.patch('/', (req, res) => {
+//     res.send("Patch")
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server running at PORT ${PORT}`)
 })
