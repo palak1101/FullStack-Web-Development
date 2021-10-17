d2abbba5e081df6034a7c6253b2e22d6aab6ab3d
Palak <palaktiwari156@gmail.com>
2021-10-17 17:00:49 +0530

    access of private route to admin only if it is true in headers

--- server.js
+++ server.js
@@ -8,32 +8,21 @@ const app = express()
 const PORT = 3000
 
 
-// 1. Headers of request contain additional info about request.
-app.get('/', (req, res) => {
-    console.log(req.headers)
-    res.send("GET")
-})
+// 1. 
 
-
-//2. Basic security to check from where(user-agent) request is coming?
-
-app.get('/', (req, res) => {
-    console.log(req.headers['user-agent'])
-    if(req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") res.send("GET")
-    else res.send("BLOCKED")
-})
-
-
-//3. Middleware - a more logical way for security check.
-
-const verify = (req, res, next) => {
-    if(req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next()
-    else res.send("BLOCKED")
+const isAdmin = (req, res, next) => {
+    if(req.headers.admin === "true") next()
+    else res.send("ACCESS DENIED!!!")
 }
 
-app.get('/', verify, (req, res) => {
-    //console.log(req)
-    res.send("Hi!")
+
+app.get('/public', (req, res) => {
+    console.log(req.headers)
+    res.send("I'm a public route")
+})
+
+app.get('/private', isAdmin, (req, res) => {
+    res.send("I'm a private route")
 })
 
 
