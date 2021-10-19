f0d8e0dd187ff2d2e0cc17538cd8f858d50fef09
Palak <palaktiwari156@gmail.com>
2021-10-19 16:25:30 +0530

    e-commerce backend - I

--- server.js
+++ server.js
@@ -1,23 +1,85 @@
+// BASIC ECOMMERCE BACKEND
 
 // create a server using express-
 
+const { json } = require('express')
 const express = require('express')
 const app = express()
-//console.log(express)
 
+//console.log(express)
 const PORT = 3000
 
-
-// 1. Signup/Login Backend-
 app.use(express.json())
 
+const token = "TOP_SECRET"
 
-app.post('/signup', (req, res) => {
-    console.log(req.body)
-    res.send("dummy website data")
+
+//Array as Database
+let products = [{name: "iPhone12 Case", price: '999'}, {name: "iPhone13 Case", price: '1199'}, {name: "iPhone13 Pro Case", price: '1499'}]
+
+
+
+
+//middleware - server validation
+//1.
+const validator = (req, res, next) => {
+    const {name, price} = req.body
+
+    if (!name || !price) res.json({error: "validation failed!"})
+    else next()
+}
+
+//2.
+const isAuthorized = (req, res, next) => {
+    if(req.headers.authorisation === token){
+        next()
+    }
+    else res.json({error: "UNAUTHORIZED"})
+}
+
+
+
+
+//-----------------------------PUBLIC ROUTE--------------------------------
+//This route is a get route to send products-
+app.get('/products', (req, res) => {
+    res.json({products})
 })
 
 
-app.listen(3000, () => {
+
+
+//-----------------------------PRIVATE ROUTE--------------------------------
+app.post('/products/add', isAuthorized, validator, (req, res) => {
+    const {name, price} = req.body   //destructure
+    
+    const newProduct = {
+        name,
+        price
+    }
+    // const newProduct = {
+    //     name: req.body.name,
+    //     price: req.body.price
+    // }
+    products.push(newProduct)
+    res.send("product added")
+})
+
+//-----------------------------AUTHENTICATION FOR ACCESSING PRIVATE ROUTE--------------------------------
+
+app.post('/auth', (req, res) => {
+    const {email, password} = req.body
+    if (email === "admin@mail.com" && password === "password1") 
+    res.send({token})
+    else{
+        res.send({message: "UNAUTHORIZED"})
+    }
+})
+
+
+
+
+
+app.listen(PORT, () => {
     console.log(`Server listening at PORT ${PORT}`)
-}) 
+})
